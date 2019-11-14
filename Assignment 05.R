6e1d7daef79bdbf15a481539169db68d9f1767a1
Marilyn Gao <myg223@lehigh.edu>
2019-11-14 17:13:22 -0500

    Added own data set, ran the code

---
+++ Assignment 05.R
@@ -0,0 +1,8 @@
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+header <- read.table("A191RL1Q225SBEA.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("A191RL1Q225SBEA.csv", skip=1, sep=",",
+            header=FALSE, data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
